c99a6f54b9929051ef0a1226270c9e4d30a3e6d5
Thomas Kühne <thomas@kuehne-webdienste.de>
2020-05-28 17:54:08 +0200

    find term for 1 source

--- theme/private/redaxo/modules/Liste nach Parameter [7]/7.Liste nach Parameter.output.php
+++ theme/private/redaxo/modules/Liste nach Parameter [7]/7.Liste nach Parameter.output.php
@@ -21,6 +21,8 @@ if (!function_exists('makeLinkList')) {
 	}
 }
 
+$detailsArticleId = 'REX_LINK[id=1 output=id]';
+
 // !!! probably you should use PHP classes/methods to evaluate it(?)
 $search = rex_request('FORM');
 if ($search) {
@@ -40,7 +42,7 @@ if ($search) {
 		$sql = rex_sql::factory();
 		$query = "SELECT id,begriff from tth_wortliste WHERE begriff LIKE '".str_replace('*','%',$searchPattern)."'";
 		?>
-		<div class="link-list"><?=makeLinkList($sql->getArray($query), 'begriff_id', 'begriff', 'REX_LINK[id=1 output=id]');?></div>
+		<div class="link-list"><?=makeLinkList($sql->getArray($query), 'begriff_id', 'begriff', $detailsArticleId);?></div>
 		<?php
 	}
 }
@@ -48,6 +50,7 @@ else {
 	$lang = rex_request('sprache_id');
 	$region = rex_request('region_id');
 	$style = rex_request('sprachstil_id');
+	$source = rex_request('quelle_id');
 
 	if ($lang || 0 === $lang || '0' === $lang) {
 		$sql = rex_sql::factory();
@@ -69,6 +72,26 @@ else {
 		$sql = rex_sql::factory();
 		?>
 		<p><strong>Begriffe nach Sprachstil <?=$style?>:</strong> <?=makeLinkList($sql->getArray("SELECT id,begriff FROM tth_wortliste WHERE sprachstil_id=$style"), 'begriff_id', 'begriff', 'REX_LINK[id=1 output=id]')?></p>
+		<?php	
+	}
+	else if ($source || 0 === $source || '0' === $source) {
+		$sql = rex_sql::factory();
+
+		$tableEntities = 'tth_wortliste';
+		$tableRelationSources = 'tth_begriff_quellen';
+		$tableSources = 'tth_quellen';
+
+		$query = "SELECT $tableEntities.begriff,$tableEntities.id
+		FROM $tableRelationSources
+	    INNER JOIN $tableEntities
+        ON $tableRelationSources.begriff_id = $tableEntities.id
+    	INNER JOIN $tableSources
+		ON $tableRelationSources.quelle_id = $tableSources.id
+		WHERE $tableSources.id = $source";
+		
+		$rows = $sql->getArray($query);
+
+		?><p><strong>Begriffe nach Quelle (ID=<?=$source?>, <?=count($rows)?> Einträge):</strong> <?=makeLinkList($rows, 'begriff_id', 'begriff', 'REX_LINK[id=1 output=id]')?></p>
 		<?php	
 	}
 	else {
@@ -94,6 +117,12 @@ else {
 		?>
 		<p><strong>Sprachstil:</strong> <?=makeLinkList($styles, 'sprachstil_id', 'stil')?></p>
 		<?php
-	}
+
+		$quellen = $sql->getArray("SELECT id,kurz FROM tth_quellen WHERE 1");
+		array_push($quellen, array( 'id' => 0, 'kurz' => 'nicht gesetzt'));
+		?>
+		<p><strong>Quelle:</strong> <?=makeLinkList($quellen, 'quelle_id', 'kurz')?></p>
+		<?php
+}
 }
 ?>
