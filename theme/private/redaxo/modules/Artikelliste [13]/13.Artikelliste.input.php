b7570862539765fddd52a2cba3699148de4431f5
Thomas Kühne <thomas@kuehne-webdienste.de>
2020-10-26 21:49:18 +0100

    styling of blog list

--- theme/private/redaxo/modules/Artikelliste [13]/13.Artikelliste.input.php
+++ theme/private/redaxo/modules/Artikelliste [13]/13.Artikelliste.input.php
@@ -1,1 +1,5 @@
 Quellkategorie der Artikel, die aufgelistet werden sollen:
+
+<hr>
+geplant: Einstellung ob Blog-Daten angezeigt werden sollen. Die Daten (wie Autor, Datum) sind ja immer vorhanden,
+werden nur nicht angezeigt.
