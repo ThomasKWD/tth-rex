214a28fa2462d35b98044772f652c084bcc8700e
Thomas Kühne <thomas@kuehne-webdienste.de>
2021-02-10 12:44:59 +0100

    output of tags filter module

--- theme/private/redaxo/modules/Begriffe fuer Schlagwort [15]/15.Begriffe fuer Schlagwort.output.php
+++ theme/private/redaxo/modules/Begriffe fuer Schlagwort [15]/15.Begriffe fuer Schlagwort.output.php
@@ -1,4 +1,24 @@
 <?php
+    // !!! should go into my helper class
+    if (!function_exists('getFieldById')) {
+          // best array_find function better than using array_filter which can not stop on find!
+        // function array_search_func(array $arr, $func) {
+        //     foreach ($arr as $key => $v)
+        //         if ($func($v))
+        //             return $key;
+
+        //     return false;
+        // }
+        function getFieldById(array $arr, $id, $fieldName) {
+            foreach ($arr as $key => $v) {
+                if ($v['id'] == $id)
+                return $v[$fieldName];
+            }
+
+            return false;
+        }
+    }
+
     // !!! provide centralized name string base!
     $tagsTable = 'tth_tags';
     $tagsRelationTable = 'tth_begriff_tags';
@@ -10,24 +30,37 @@
 
     // makes clickable List:
     $tm = new \kwd\tth\TableManager();
+    $tag = 'Schlagwort';
 
     if ($tagsArray && count($tagsArray)) {
-        $html = $tm->makeLinkList($tagsArray, 'tag_id', 'name');
-        echo $html;
+        echo '<p>' . $tm->makeLinkList($tagsArray, 'tag_id', 'name') .'</p>';
     }
-
+    
     if (rex_request('tag_id')) {
         $id = rex_escape(rex_request('tag_id','string'));
         // for($i=0; $<count($))
+        // echo "id: $id";
+        $tag = getFieldById($tagsArray, $id, 'name');
+
+        $tableEntities = 'tth_wortliste';
+        $tableRelationTags = 'tth_begriff_tags';
+
+        // !!! inner join of outer relation (just like in `DetailAnsicht`)
+        //     should go into tablemanager!!
+        // !!! could make "getOuterRelation in table manager
+        $entitiesQuery = "SELECT r.tag_id, r.begriff_id, e.begriff ";
+        $entitiesQuery.= "FROM $tableRelationTags r ";
+        $entitiesQuery.= "JOIN $tableEntities e ON r.begriff_id = e.id ";
+        $entitiesQuery.= "WHERE r.tag_id=$id ORDER BY e.begriff ASC";
+        $entities = $sql->getArray($entitiesQuery);
+        // dump($entities);
+        if (count($entities)) {
+            echo "<h3>Begriffe, die \"$tag\" enthalten</h3>";
+            echo '<p>'.$tm->makeLinkList($entities, 'begriff_id', 'begriff').'</p>';
+        }
+        else {
+            echo "<p>Keine Begriffe mit \"$tag\" gefunden.";
+        }
     }
 
-    // best array_find function better than using array_filter which can not stop on find!
-    //     function array_search_func(array $arr, $func)
-    // {
-    //     foreach ($arr as $key => $v)
-    //         if ($func($v))
-    //             return $key;
-
-    //     return false;
-    // }
-?>
+  ?>
