3fc22c0598053b5f1a3a866d7263fb90d1da8abf
Thomas Kühne <thomas@kuehne-webdienste.de>
2021-03-11 18:34:44 +0100

    refactored for code separation

--- theme/private/redaxo/modules/Begriffe fuer Schlagwort [15]/15.Begriffe fuer Schlagwort.output.php
+++ theme/private/redaxo/modules/Begriffe fuer Schlagwort [15]/15.Begriffe fuer Schlagwort.output.php
@@ -22,57 +22,34 @@
     if (!isset($sql)) $sql = rex_sql::factory();
     if (!isset($vm)) $vm = new \kwd\tth\ViewFormatter($sql, 'rex_getUrl'); 
 
-    // !!! put into tm/vm
-
     $tm = $vm->getTableManagerInstance();
     $tagsArray = $tm->getOuterRelationTableData('tags');
 
-    // // !!! provide centralized name string base!
-    // $tagsTable = 'tth_tags';
-    // $tagsRelationTable = 'tth_begriff_tags';
-
-
-    // $listQuery = "SELECT id, name FROM $tagsTable ORDER BY name ASC";
-    // $tagsArray = $sql->getArray($listQuery);
-
-    // // makes clickable List:
-    // $tm = new \kwd\tth\TableManager();
     $tag = 'Schlagwort';
 
     if ($tagsArray && count($tagsArray)) {
         echo '<p>' . $vm->getLinkList($tagsArray, 'tag_id', 'name') .'</p>';
+        // !!! make getLinkListBySubject('<subject>')
     }
 
+    // tag_id is equal to field name in DB, but is not required, could also be different by convention
     if (rex_request('tag_id')) {
         $id = rex_escape(rex_request('tag_id','string'));
         // for($i=0; $<count($))
         // echo "id: $id";
         $tag = getFieldById($tagsArray, $id, 'name');
 
-        $tableEntities = 'tth_wortliste';
-        $tableRelationTags = 'tth_begriff_tags';
-
-        // !!! inner join of outer relation (just like in `DetailAnsicht`)
-        //     should go into tablemanager!!
-        // !!! could make "getOuterRelation in table manager
-        $entitiesQuery = "SELECT r.tag_id, r.begriff_id, e.begriff, e.id "; // e.id for output below in makeLinkList
-        $entitiesQuery.= "FROM $tableRelationTags r ";
-        $entitiesQuery.= "JOIN $tableEntities e ON r.begriff_id = e.id ";
-        $entitiesQuery.= "WHERE r.tag_id=$id ORDER BY e.begriff ASC";
-        $entities = $sql->getArray($entitiesQuery);
-        // dump($entities);
-        if (count($entities)) {
+        $listHtml = $vm->getEntityLinkListForOuterRelation('tags', $id, "REX_LINK[id=1 output=id]");
+        if ($listHtml) {
             // dump($entities);
             echo "<h3>Begriffe, die \"$tag\" enthalten</h3>";
-            echo '<p>'.$vm->getLinkList($entities, 'begriff_id', 'begriff',"REX_LINK[id=1 output=id]").'</p>';
+            echo '<p>'.$listHtml.'</p>';
         }
         else {
             echo "<p>Keine Begriffe mit \"$tag\" gefunden.";
         }
 
         echo "<hr><p>Begriffe, bei denen \"Kategorie\" gesetzt ist</p>";
-        $katEntitiesQuery = "SELECT id, begriff FROM $tableEntities WHERE kategorie = 'TRUE' ORDER BY begriff ASC";
-        $kats = $sql->getArray($katEntitiesQuery);
-        echo '<p>'.$vm->getLinkList($kats, 'begriff_id', 'begriff',"REX_LINK[id=1 output=id]").'</p>';
+        echo '<p>'.$vm->getEntityLinkListByFieldValue('is_category', 'TRUE', "REX_LINK[id=1 output=id]").'</p>';
     }
   ?>
